be9b2a5e2d87118cf98cb9798a0633b1fb3c844b
Mikael Gråborg <mgraborg@gmail.com>
2025-10-01 22:45:32 +0200

    chore: bump rustler

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule ExTypst.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.28.0"},
+      {:rustler, "~> 0.30.0"},
       {:benchee, "~> 1.0", only: :dev},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
